f6175dfcbf2983eb7319db243b882c8bb21ec982
Fabien Potencier <fabien.potencier@gmail.com>
2011-02-20 10:13:18 +0100

    fixed typo

--- DependencyInjection/SensioFrameworkExtraExtension.php
+++ DependencyInjection/SensioFrameworkExtraExtension.php
@@ -2,7 +2,7 @@
 
 namespace Sensio\Bundle\FrameworkExtraBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
