88fff1c8fe70c09815fa32af359b84d99248cdc7
Henrik Bjørnskov <henrik@bearwoods.dk>
2011-11-21 10:52:01 +0100

    travis-ci.org configuration

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,12 +1,21 @@
 <?php
 
-require_once $_SERVER['SYMFONY'] . '/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+if (!@include __DIR__ . '/../vendor/.composer/autoload.php') {
+    die(<<<'EOT'
+You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+EOT
+    );
+}
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Sensio\Bundle\FrameworkExtraBundle' => __DIR__ . '/../../',
-    'Symfony' => $_SERVER['SYMFONY'],
-));
-$loader->register();
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'Sensio\\Bundle\\FrameworkExtraBundle\\')) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
+    }
+});
