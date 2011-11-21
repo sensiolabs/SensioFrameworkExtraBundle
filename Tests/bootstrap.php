b35c9d2b18dc86172e7f7e630b244df18cbf17f4
Henrik Bjørnskov <henrik@bearwoods.dk>
2011-11-21 10:59:24 +0100

    Remove composer from autoload in Tests/bootstrap.php

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,14 +1,5 @@
 <?php
 
-if (!@include __DIR__ . '/../vendor/.composer/autoload.php') {
-    die(<<<'EOT'
-You must set up the project dependencies, run the following commands:
-wget http://getcomposer.org/composer.phar
-php composer.phar install
-EOT
-    );
-}
-
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Sensio\\Bundle\\FrameworkExtraBundle\\')) {
         $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
