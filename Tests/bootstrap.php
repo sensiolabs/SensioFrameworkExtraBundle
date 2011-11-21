0c7aec68b7783b103b7aaf0fdcc74ff03e1306c1
Henrik Bjørnskov <henrik@bearwoods.dk>
2011-11-21 11:02:17 +0100

    Fix bootstrap autoloading

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,10 +2,12 @@
 
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Sensio\\Bundle\\FrameworkExtraBundle\\')) {
-        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        $path = __DIR__.'/../../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+
         if (!stream_resolve_include_path($path)) {
             return false;
         }
+
         require_once $path;
         return true;
     }
