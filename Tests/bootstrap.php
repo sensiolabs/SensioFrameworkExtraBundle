7b6a629358e9d18736f242d3c49bdbc444d5c952
Henrik Bjørnskov <henrik@bjrnskov.dk>
2011-11-28 12:57:21 +0100

    fix bootstrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,21 +1,13 @@
 <?php
 
 spl_autoload_register(function($class) {
-    if (0 === strpos($class, 'Sensio\\Bundle\\FrameworkExtraBundle\\')) {
-        $path = __DIR__.'/../../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
-
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-
-
-spl_autoload_register(function($class) {
     if (0 === (strpos($class, 'Sensio\\Bundle\\FrameworkExtraBundle\\'))) {
         $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 3)).'.php';
 
         if (!stream_resolve_include_path($path)) {
             return false;
         }
+
         require_once $path;
         return true;
     }
