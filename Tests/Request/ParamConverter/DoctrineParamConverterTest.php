ef03a8913d064cc5c4b6b3536ab095535cd304fb
Tarjei Huse <tarjei@scanmine.com>
2012-01-05 14:59:10 +0100

    This commit changes the DoctrineParamConverter to handle paths like:
    /{user}/{group}
    
    by beeing able to assign which request variable should be used for a
    given provider.
    
    It also adds support for defining how the object should be queried.

--- Tests/Request/ParamConverter/DoctrineParamConverterTest.php
+++ Tests/Request/ParamConverter/DoctrineParamConverterTest.php
@@ -19,19 +19,39 @@ class DoctrineParamConverterTest extends \PHPUnit_Framework_TestCase
     
     public function setUp()
     {
+<<<<<<< HEAD
         if (!interface_exists('Doctrine\Common\Persistence\ManagerRegistry')) {
             $this->markTestSkipped();
+=======
+            if (!interface_exists('Doctrine\Common\Persistence\ManagerRegistry')) {
+            $this->markTestSkipped('Missing Doctrine\Common\Persistence\ManagerRegistry');
+>>>>>>> 62cf316... This commit changes the DoctrineParamConverter to handle paths like:
         }
 
         $this->manager = $this->getMock('Doctrine\Common\Persistence\ManagerRegistry');
         $this->converter = new DoctrineParamConverter($this->manager);
+<<<<<<< HEAD
+=======
+        
+        $this->objectRepository = $this->getMock('Doctrine\Common\Persistence\ObjectRepository');
+        $this->objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
+        $this->manager->expects($this->any())->method('getRepository')
+                      ->will($this->returnValue($this->objectRepository));
+        $this->manager->expects($this->any())->method('getManager')
+                      ->will($this->returnValue($this->objectManager));
+        
+>>>>>>> 62cf316... This commit changes the DoctrineParamConverter to handle paths like:
     }
     
     public function createConfiguration($class = null, array $options = null)
     {
         $config = $this->getMock(
             'Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface', array(
+<<<<<<< HEAD
             'getClass', 'getAliasName', 'getOptions'
+=======
+            'getClass', 'getAliasName', 'getOptions', 'isOptional', 'getName', 
+>>>>>>> 62cf316... This commit changes the DoctrineParamConverter to handle paths like:
         ));
         if ($options !== null) {
             $config->expects($this->once())
@@ -50,6 +70,7 @@ class DoctrineParamConverterTest extends \PHPUnit_Framework_TestCase
     {
         $request = new Request();
         $config = $this->createConfiguration(null, array());
+<<<<<<< HEAD
         $objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
         
         $this->manager->expects($this->never())->method('find');
@@ -61,6 +82,82 @@ class DoctrineParamConverterTest extends \PHPUnit_Framework_TestCase
         $this->converter->apply($request, $config);
     }
     
+=======
+        $this->setExpectedException('LogicException');
+        $this->converter->apply($request, $config);
+    }
+
+    public function testNonOptionalNotFound()
+    {
+        $request = new Request();
+        $config = $this->createConfiguration(null, array());
+        $config->expects($this->once())
+               ->method("isOptional")
+               ->will($this->returnValue(false));
+                
+        $request->attributes->set("id", "34");
+        
+        $this->setExpectedException('Symfony\Component\HttpKernel\Exception\NotFoundHttpException');
+        $this->converter->apply($request, $config);
+    }
+    /**
+     * Old behaviour, we use the id attribute to query for the user by id
+     */
+    public function testApplyDefault()
+    {
+        $request = new Request();
+        $config = $this->createConfiguration("User", array());
+        
+        $request->attributes->set("id", "testName");
+        
+        $this->objectRepository->expects($this->once())->method('find')
+                ->with($this->equalTo("testName"))->will($this->returnValue("object"))
+                ;
+        $this->objectRepository->expects($this->never())->method('findBy');
+        
+        $ret = $this->converter->apply($request, $config);
+        
+        $this->assertTrue($ret, "We should have found an object");
+        $this->assertEquals("object", $request->attributes->get($config->getName()));
+    }
+
+    public function testApplyWithRequestAttribute()
+    {
+        $request = new Request();
+        $config = $this->createConfiguration("User", array('request_attribute' => 'name'));
+        
+        $request->attributes->set("name", "testName");
+        
+        $this->objectRepository->expects($this->once())->method('find')
+                ->with($this->equalTo("testName"))->will($this->returnValue("object"))
+                ;
+        $this->objectRepository->expects($this->never())->method('findBy');
+        
+        $ret = $this->converter->apply($request, $config);
+        
+        $this->assertTrue($ret, "We should have found an object");
+        $this->assertEquals("object", $request->attributes->get($config->getName()));
+    }
+
+        public function testApplyWithRequestAttributeAndQueryAttribute()
+    {
+        $request = new Request();
+        $config = $this->createConfiguration("User", array('request_attribute' => 'name'));
+        
+        $request->attributes->set("name", "testName");
+        
+        $this->objectRepository->expects($this->once())->method('find')
+                ->with($this->equalTo("testName"))->will($this->returnValue("object"))
+                ;
+        $this->objectRepository->expects($this->never())->method('findBy');
+        
+        $ret = $this->converter->apply($request, $config);
+        
+        $this->assertTrue($ret, "We should have found an object");
+        $this->assertEquals("object", $request->attributes->get($config->getName()));
+    }
+
+>>>>>>> 62cf316... This commit changes the DoctrineParamConverter to handle paths like:
     public function testSupports()
     {
         $config = $this->createConfiguration('stdClass', array());
@@ -70,18 +167,33 @@ class DoctrineParamConverterTest extends \PHPUnit_Framework_TestCase
                         ->with($this->equalTo('stdClass'))
                         ->will($this->returnValue( false ));
         
+<<<<<<< HEAD
         $objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
         $objectManager->expects($this->once())
+=======
+        $this->objectManager->expects($this->once())
+>>>>>>> 62cf316... This commit changes the DoctrineParamConverter to handle paths like:
                       ->method('getMetadataFactory')
                       ->will($this->returnValue($metadataFactory));
         
         $this->manager->expects($this->once())
                       ->method('getManager')
                       ->with($this->equalTo('default'))
+<<<<<<< HEAD
                       ->will($this->returnValue($objectManager));
         
         $ret = $this->converter->supports($config);
         
         $this->assertTrue($ret, "Should be supported");
+=======
+                      ->will($this->returnValue($this->objectManager));
+        
+        $ret = $this->converter->supports($config);
+        
+        
+        
+        $this->assertTrue($ret, "Should be supported");
+        
+>>>>>>> 62cf316... This commit changes the DoctrineParamConverter to handle paths like:
     }
 }
