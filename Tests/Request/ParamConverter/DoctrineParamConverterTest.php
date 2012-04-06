cd8dcfd7a7a81f22ff0b9a4c5749855428e66f32
Lennart Hildebrandt <hello@lennerd.com>
2012-04-06 20:19:15 +0200

    Added test for the new DoctrineParamConverter abilites

--- Tests/Request/ParamConverter/DoctrineParamConverterTest.php
+++ Tests/Request/ParamConverter/DoctrineParamConverterTest.php
@@ -7,60 +7,118 @@ use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\DoctrineParamConve
 
 class DoctrineParamConverterTest extends \PHPUnit_Framework_TestCase
 {
+    const PARAMETER_NAME = 'test';
+
     /**
-     * @var Doctrine\Common\Persistence\ManagerRegistry
+     * @var \Doctrine\Common\Persistence\ManagerRegistry
      */
-    private $manager;
-    
+    private $registry;
+
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @var \Doctrine\Common\Persistence\Mapping\ClassMetadataFactory
+     */
+    private $metadataFactory;
+
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectRepository
+     */
+    private $repository;
+
+    /**
+     * @var \Doctrine\Common\Persistence\Mapping\ClassMetadata
+     */
+    private $metadata;
+
     /**
      * @var DoctrineParamConverter
      */
     private $converter;
-    
+
     public function setUp()
     {
         if (!interface_exists('Doctrine\Common\Persistence\ManagerRegistry')) {
             $this->markTestSkipped();
         }
 
-        $this->manager = $this->getMock('Doctrine\Common\Persistence\ManagerRegistry');
-        $this->converter = new DoctrineParamConverter($this->manager);
+        $this->registry  = $this->getMock('Doctrine\Common\Persistence\ManagerRegistry');
+        $this->converter = new DoctrineParamConverter($this->registry);
+
+        $this->metadataFactory = $this->getMock('Doctrine\Common\Persistence\Mapping\ClassMetadataFactory');
+
+        $this->objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
+        $this->objectManager->expects($this->any())
+            ->method('getMetadataFactory')
+            ->will($this->returnValue($this->metadataFactory));
+
+        $this->metadata = $this->getMock('Doctrine\Common\Persistence\Mapping\ClassMetadata');
+
+        $this->metadata->expects($this->any())
+            ->method('hasField')
+            ->will($this->returnCallback(function($name) {
+            if ('user' === $name) {
+                return true;
+            }
+
+            return false;
+        }));
+
+        $this->metadata->expects($this->any())
+            ->method('hasAssociation')
+            ->will($this->returnCallback(function($name) {
+            if ('group' === $name) {
+                return true;
+            }
+
+            return false;
+        }));
+
+        $this->objectManager->expects($this->any())
+            ->method('getClassMetadata')
+            ->with($this->equalTo('stdClass'))
+            ->will($this->returnValue($this->metadata));
+
+        $this->registry->expects($this->any())
+            ->method('getManager')
+            ->will($this->returnValue($this->objectManager));
+
+        $this->repository = $this->getMock('Doctrine\Common\Persistence\ObjectRepository');
+
+        $this->registry->expects($this->any())
+            ->method('getRepository')
+            ->with($this->equalTo('stdClass'))
+            ->will($this->returnValue($this->repository));
     }
-    
-    public function createConfiguration($class = null, $name = null, array $options = null)
+
+    protected function createConfiguration(array $options = array())
     {
-        $config = $this->getMock(
-            'Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface', array(
-            'getClass', 'getAliasName', 'getName', 'getOptions',
+        $config = $this->getMock('Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface', array(
+            'getClass', 'getAliasName', 'getName', 'getOptions', 'isOptional'
         ));
-        if ($options !== null) {
-            $config->expects($this->once())
-                   ->method('getOptions')
-                   ->will($this->returnValue($options));
-        }
-        if ($class !== null) {
-            $config->expects($this->any())
-                   ->method('getClass')
-                   ->will($this->returnValue($class));
-        }
-        if ($name !== null) {
-            $config->expects($this->any())
-                   ->method('getName')
-                   ->will($this->returnValue($name));
-        }
+
+        $config->expects($this->once())
+            ->method('getOptions')
+            ->will($this->returnValue($options));
+
+        $config->expects($this->any())
+            ->method('getClass')
+            ->will($this->returnValue('stdClass'));
+
+        $config->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue(self::PARAMETER_NAME));
+
         return $config;
     }
 
     public function testApplyWithNoIdAndData()
     {
         $request = new Request();
-        $config = $this->createConfiguration(null, 'test', array());
-        $objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
-
-        $this->manager->expects($this->never())->method('find');
-        $this->manager->expects($this->once())
-                      ->method('getManager')
-                      ->will($this->returnValue($objectManager));
+        $config  = $this->createConfiguration();
 
         $this->setExpectedException('LogicException');
         $this->converter->apply($request, $config);
@@ -68,25 +126,127 @@ class DoctrineParamConverterTest extends \PHPUnit_Framework_TestCase
 
     public function testSupports()
     {
-        $config = $this->createConfiguration('stdClass', 'test', array());
-        $metadataFactory = $this->getMock('Doctrine\Common\Persistence\Mapping\ClassMetadataFactory');
-        $metadataFactory->expects($this->once())
-                        ->method('isTransient')
-                        ->with($this->equalTo('stdClass'))
-                        ->will($this->returnValue( false ));
-        
-        $objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
-        $objectManager->expects($this->once())
-                      ->method('getMetadataFactory')
-                      ->will($this->returnValue($metadataFactory));
-        
-        $this->manager->expects($this->once())
-                      ->method('getManager')
-                      ->with($this->equalTo('default'))
-                      ->will($this->returnValue($objectManager));
-        
+        $config = $this->createConfiguration();
+
+        $this->metadataFactory->expects($this->once())
+            ->method('isTransient')
+            ->with($this->equalTo('stdClass'))
+            ->will($this->returnValue(false));
+
         $ret = $this->converter->supports($config);
-        
+
         $this->assertTrue($ret, "Should be supported");
     }
+
+    public function testApplyWithId()
+    {
+        $request = new Request();
+        $config  = $this->createConfiguration();
+
+        $request->attributes->set('id', '42');
+
+        $entity = new \stdClass();
+
+        $this->repository->expects($this->once())
+            ->method('find')
+            ->with($this->equalTo('42'))
+            ->will($this->returnValue($entity));
+
+        $this->assertTrue($this->converter->apply($request, $config));
+        $this->assertEquals($entity, $request->attributes->get(self::PARAMETER_NAME));
+    }
+
+    public function testApplyWithName()
+    {
+        $request = new Request();
+        $config  = $this->createConfiguration();
+
+        $request->attributes->set(self::PARAMETER_NAME, '42');
+
+        $entity = new \stdClass();
+
+        $this->repository->expects($this->once())
+            ->method('find')
+            ->with($this->equalTo('42'))
+            ->will($this->returnValue($entity));
+
+        $this->assertTrue($this->converter->apply($request, $config));
+        $this->assertEquals($entity, $request->attributes->get(self::PARAMETER_NAME));
+    }
+
+    public function testApplyWithCriterias()
+    {
+        $request = new Request();
+        $config  = $this->createConfiguration();
+
+        $criterias = array(
+            'user' => 1,
+            'group' => 2,
+        );
+
+        $request->attributes->replace($criterias);
+
+        $entity = new \stdClass();
+
+        $this->repository->expects($this->once())
+            ->method('findOneBy')
+            ->with($this->equalTo(array(
+                'user' => 1,
+                'group' => 2,
+            )))
+            ->will($this->returnValue($entity));
+
+        $this->assertTrue($this->converter->apply($request, $config));
+        $this->assertEquals($entity, $request->attributes->get(self::PARAMETER_NAME));
+    }
+
+    public function testApplyWithPrefixedCriterias()
+    {
+        $request = new Request();
+        $config  = $this->createConfiguration();
+
+        $request->attributes->replace(array(
+            self::PARAMETER_NAME . '_user' => 1,
+            self::PARAMETER_NAME . '_group' => 2,
+        ));
+
+        $entity = new \stdClass();
+
+        $this->repository->expects($this->once())
+            ->method('findOneBy')
+            ->with($this->equalTo(array(
+                'user' => 1,
+                'group' => 2,
+            )))
+            ->will($this->returnValue($entity));
+
+        $this->assertTrue($this->converter->apply($request, $config));
+        $this->assertEquals($entity, $request->attributes->get(self::PARAMETER_NAME));
+    }
+
+    public function testApplyWithCustomRepositoryMethod()
+    {
+        $this->markTestIncomplete('Need a way to test the reflection process.');
+    }
+
+    public function testApplyWithOptional()
+    {
+        $request = new Request();
+        $config  = $this->createConfiguration();
+
+        $request->attributes->set('id', '42');
+
+        $config->expects($this->once())
+            ->method('isOptional')
+            ->will($this->returnValue(false));
+
+        $this->repository->expects($this->once())
+            ->method('find')
+            ->with($this->equalTo('42'))
+            ->will($this->returnValue(null));
+
+        $this->setExpectedException('Symfony\Component\HttpKernel\Exception\NotFoundHttpException');
+        $this->converter->apply($request, $config);
+    }
+
 }
