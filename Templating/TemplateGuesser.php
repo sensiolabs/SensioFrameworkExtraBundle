d3eaadf1d6fe5296e9dc6a665cb0bc4fb863832b
Francis Besset <francis.besset@sensio.com>
2012-09-20 16:11:41 +0200

    Fixed typo for the cascading parent bundle

--- Templating/TemplateGuesser.php
+++ Templating/TemplateGuesser.php
@@ -54,21 +54,24 @@ class TemplateGuesser
 
         if (!preg_match('/Controller\\\(.+)Controller$/', $className, $matchController)) {
             throw new \InvalidArgumentException(sprintf('The "%s" class does not look like a controller class (it must be in a "Controller" sub-namespace and the class name must end with "Controller")', get_class($controller[0])));
-
         }
         if (!preg_match('/^(.+)Action$/', $controller[1], $matchAction)) {
             throw new \InvalidArgumentException(sprintf('The "%s" method does not look like an action method (it does not end with Action)', $controller[1]));
         }
 
         $bundle = $this->getBundleForClass($className);
-        $parentBundleName = $bundle->getName();
-        while (null !== $bundleName = $bundle->getParent()) {
-            $parentBundleName = $bundleName;
-            $bundles = $this->kernel->getBundle($bundleName, false);
+        while ($bundleName = $bundle->getName()) {
+            if (null === $parentBundleName = $bundle->getParent()) {
+                $bundleName = $bundle->getName();
+
+                break;
+            }
+
+            $bundles = $this->kernel->getBundle($parentBundleName, false);
             $bundle = array_pop($bundles);
         }
 
-        return new TemplateReference($parentBundleName, $matchController[1], $matchAction[1], $request->getRequestFormat(), $engine);
+        return new TemplateReference($bundleName, $matchController[1], $matchAction[1], $request->getRequestFormat(), $engine);
     }
 
     /**
