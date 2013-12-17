43c8656466597713404d3ceaa66fb1dffce4f498
Jérémy JOURDIN <jjourdin.externe@m6.fr>
2013-12-17 17:50:30 +0100

    Allow cache on 301, 302, 404, 410 HTTP Codes

--- EventListener/HttpCacheListener.php
+++ EventListener/HttpCacheListener.php
@@ -88,7 +88,7 @@ class HttpCacheListener implements EventSubscriberInterface
 
         $response = $event->getResponse();
 
-        if (!$response->isSuccessful()) {
+        if (!$response->isSuccessful() && !$response->isCacheable()) {
             return;
         }
 
