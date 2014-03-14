af3f2db100391748a23c2357c5f7c4ac5edfb937
Fabien Potencier <fabien.potencier@gmail.com>
2014-03-14 09:29:45 +0100

    added some unit tests for the auto-convert flag

--- Tests/EventListener/ParamConverterListenerTest.php
+++ Tests/EventListener/ParamConverterListenerTest.php
@@ -15,23 +15,57 @@ class ParamConverterListenerTest extends \PHPUnit_Framework_TestCase
         $kernel = $this->getMock('Symfony\Component\HttpKernel\HttpKernelInterface');
         $request = new Request();
 
+        $listener = new ParamConverterListener($this->getParamConverterManager($request, array()));
+        $event = new FilterControllerEvent($kernel, array(new TestController(), 'noArgAction'), $request, null);
+
+        $listener->onKernelController($event);
+    }
+
+    public function testAutoConvert()
+    {
+        $kernel = $this->getMock('Symfony\Component\HttpKernel\HttpKernelInterface');
+        $request = new Request(array(), array(), array('date' => '2014-03-14 09:00:00'));
+
+        $converter = new ParamConverter(array('name' => 'date', 'class' => 'DateTime'));
+
+        $listener = new ParamConverterListener($this->getParamConverterManager($request, array('date' => $converter)));
+        $event = new FilterControllerEvent($kernel, array(new TestController(), 'dateAction'), $request, null);
+
+        $listener->onKernelController($event);
+    }
+
+
+    public function testNoAutoConvert()
+    {
+        $kernel = $this->getMock('Symfony\Component\HttpKernel\HttpKernelInterface');
+        $request = new Request(array(), array(), array('date' => '2014-03-14 09:00:00'));
+
+        $listener = new ParamConverterListener($this->getParamConverterManager($request, array()), false);
+        $event = new FilterControllerEvent($kernel, array(new TestController(), 'dateAction'), $request, null);
+
+        $listener->onKernelController($event);
+    }
+
+    protected function getParamConverterManager(Request $request, $configurations)
+    {
         $manager = $this->getMock('Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterManager');
         $manager
             ->expects($this->once())
             ->method('apply')
-            ->with($this->equalTo($request), $this->equalTo(array()))
+            ->with($this->equalTo($request), $this->equalTo($configurations))
         ;
 
-        $listener = new ParamConverterListener($manager);
-        $event = new FilterControllerEvent($kernel, array(new TestController(), 'noArgAction'), $request, null);
-
-        $listener->onKernelController($event);
+        return $manager;
     }
 }
 
 class TestController
 {
     public function noArgAction(Request $request)
+    {
+    }
+
+    public function dateAction(\DateTime $date)
     {
     }
 }
