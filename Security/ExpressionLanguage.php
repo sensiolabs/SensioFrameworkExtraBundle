0777a200cd10ad8bbd67b28890cdae6d5c33d32c
Christophe Coevoet <stof@notk.org>
2014-07-15 16:05:30 +0200

    Fixed the compiled version of the is_granted expression function

--- Security/ExpressionLanguage.php
+++ Security/ExpressionLanguage.php
@@ -24,8 +24,8 @@ class ExpressionLanguage extends BaseExpressionLanguage
     {
         parent::registerFunctions();
 
-        $this->register('is_granted', function ($attributes, $object = null) {
-            return '$security_context->isGranted($attributes, $object)';
+        $this->register('is_granted', function ($attributes, $object = 'null') {
+            return sprintf('$security_context->isGranted(%s, %s)', $attributes, $object);
         }, function (array $variables, $attributes, $object = null) {
             return $variables['security_context']->isGranted($attributes, $object);
         });
