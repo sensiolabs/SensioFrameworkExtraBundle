bd7e329d99d01acbc36876b14c916cfefdee6d26
Fabien Potencier <fabien.potencier@gmail.com>
2017-01-31 16:39:14 -0800

    fixed typo

--- Request/ParamConverter/DoctrineParamConverter.php
+++ Request/ParamConverter/DoctrineParamConverter.php
@@ -46,7 +46,7 @@ class DoctrineParamConverter implements ParamConverterInterface
         $class = $configuration->getClass();
         $options = $this->getOptions($configuration);
 
-        if (null === $request->attributes->get($name, false)) {
+        if (null === $request->attributes->get($name)) {
             $configuration->setIsOptional(true);
         }
 
