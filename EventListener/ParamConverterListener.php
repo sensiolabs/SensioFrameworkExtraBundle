4374de40337145abaddb071ab0b2b235cefed4ce
Jaik Dean <jaik@airtimerewards.com>
2018-12-11 16:16:33 +0000

    Fix checking of invokable object controllers in ParamConverterListener

--- EventListener/ParamConverterListener.php
+++ EventListener/ParamConverterListener.php
@@ -64,7 +64,7 @@ class ParamConverterListener implements EventSubscriberInterface
 
         if (\is_array($controller)) {
             $r = new \ReflectionMethod($controller[0], $controller[1]);
-        } elseif (\is_object($controller) && \is_callable($controller, '__invoke')) {
+        } elseif (\is_object($controller) && \is_callable([$controller, '__invoke'])) {
             $r = new \ReflectionMethod($controller, '__invoke');
         } else {
             $r = new \ReflectionFunction($controller);
