8e851d7dd6dc1caed1347ff4a11e78e8b03b4bcb
Richard van Laak <richard@scribbr.nl>
2020-06-12 13:03:29 +0200

    Revert unwanted change

--- src/EventListener/HttpCacheListener.php
+++ src/EventListener/HttpCacheListener.php
@@ -15,7 +15,6 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\KernelEvent;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
@@ -79,7 +78,7 @@ class HttpCacheListener implements EventSubscriberInterface
     /**
      * Modifies the response to apply HTTP cache headers when needed.
      */
-    public function onKernelResponse(ResponseEvent $event)
+    public function onKernelResponse(KernelEvent $event)
     {
         $request = $event->getRequest();
 
